1f881705df6417fa7f0741ab7b9d08f7b0052db0
Nataly K <doublejanus4@gmail.com>
2022-08-29 22:05:16 +0300

    Homework6

---
+++ Ass1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Enter numbers divided by a comma: ");
+
+char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+
+string text = Console.ReadLine();
+
+string[] words = text.Split(delimiterChars);
+int[] numbers = Array.ConvertAll(words, int.Parse);
+int count = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > 0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine($"You entered {count} number(s) larger than zero");
+
+
+
+
+
---
+++ Ass2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+double b1 = ReadInt("Enter b1 value: ");
+double k1 = ReadInt("Enter k1 value: ");
+double b2 = ReadInt("Enter b2 value: ");
+double k2 = ReadInt("Enter k2 value: ");
+if(b1 == b2 && k1 == k2)
+{
+    Console.WriteLine("The lines are parallel");
+    return;
+}
+double x = (b2-b1)/k1-k2;
+double y = k2*x+b2;
+double tempX = 1;
+double tempY = 2;
+int count = 0;
+
+    if(tempY == k1*tempX+b1 && tempY == k2*x+b2)
+    {
+    while(count < 5 && tempY == k1*tempX+b1 && tempY == k2*x+b2)
+{
+    tempX++;
+    tempY++;
+    count++;
+
+}
+    Console.WriteLine("The lines coincide");
+    return;
+}
+
+Console.WriteLine($"The coordinates of the intersection point are {x} , {y}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
